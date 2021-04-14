773ea5b2ff570b9c320ca1a896a0c0a8e3f68a33
Matěj Kohútek <matej.kohutek@gmail.com>
2021-04-14 18:46:39 +0200

    Change return True when sent, and False when message not sent to the server
    - reason could be the server is offline

--- socket_client.py
+++ socket_client.py
@@ -1,23 +1,28 @@
-import socketio, time
+﻿import socketio, time
 
 class socket_client:
     @staticmethod
-    def __connect_eventhandler(sio, state):
+    def __connect_eventhandler(sio, state, sent):
             sio.emit('bot-light', state)
+            sent = True
             time.sleep(0.2)
             sio.disconnect()
     @staticmethod
     def __connect_error_eventhandler(sio, err):
-        print(err.message)
         sio.disconnect()
     @staticmethod
-    def switch_light(server_address, state: int = 0):
+    async def switch_light(server_address, state: int = 0):
+        sent = False
         state = min(max(int(state), 0), 1)
         sio = socketio.Client()
-        sio.on("connect", lambda : socket_client.__connect_eventhandler(sio, state))
+        sio.on("connect", lambda : socket_client.__connect_eventhandler(sio, state, sent))
         sio.on("connect_error", lambda err: socket_client.__connect_error_eventhandler(sio, err))
-        sio.connect(server_address)
-        sio.wait()
+        try:
+            sio.connect(server_address)
+            sio.wait()
+        finally:
+            return sent
+        
 
 if __name__ == "__main__":
     import sys
